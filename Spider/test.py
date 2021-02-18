0ba432283c2463bc3cb74d26ea0d6bab77570f4d
csong03 <72874056+csong03@users.noreply.github.com>
2021-02-18 22:52:05 +0800

    Update test.py

--- Spider/test.py
+++ Spider/test.py
@@ -11,29 +11,22 @@ chromedriver = "/usr/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
 
-USERNAME: ${{ secrets.USERNAME }}  # 账号
-PASSWORD: ${{ secrets.PASSWORD }}  # 密码
-login_url = 'https://hacpai.com/login?goto=https%3A%2F%2Fhacpai.com%2F'  # 登录URL
-checkin_url = 'https://hacpai.com/activity/checkin'  # 签到URL
+USERNAME: 'jgku110764@163.com'  # 账号
+PASSWORD: 'ys0106080732'  # 密码
+login_url = 'https://acgwcy.com/'  # 登录URL
+checkin_url = 'https://acgwcy.com/mission/today'  # 签到URL
 
 driver.get(login_url)  # 进入登录页面
-
-try:
-    driver.find_element_by_xpath(
-        "//*[@id='verifyHacpaiIcon' and @class='fn__flex verify__via icon-hacpai']").click()  # 使用社区账号登录
-    time.sleep(1)  # 延时加载
-    driver.find_element_by_id('nameOrEmail').send_keys(USERNAME)  # 填充用户名和密码
-    driver.find_element_by_id('loginPassword').send_keys(PASSWORD)
-    driver.find_element_by_id('loginBtn').click()  # 登录
-    time.sleep(1.5)
-    driver.get(checkin_url)
-    try:  # 未签到
-        driver.find_element_by_xpath("//*[@class='btn green']").click()  # 签到
-        print("签到成功")
-    except NoSuchElementException:
-        print("已签到")
-except Exception as e:
-    print(e)
-    print("签到失败")
+driver.find_element_by_xpath('//*[@id="page"]/div[1]/div/div[2]/div/div[2]/div[3]/div[2]/div/div[1]/button[1]').click()  # 使用社区账号登录
+time.sleep(2)  # 延时加载
+driver.find_element_by_xpath('//*[@id="login-box"]/div/div/div/form/div[2]/label[2]/input').send_keys(USERNAME)# 填充用户名和密码
+time.sleep(1) 
+driver.find_element_by_xpath('//*[@id="login-box"]/div/div/div/form/div[2]/label[5]/input').send_keys(PASSWORD)
+time.sleep(1) 
+driver.find_element_by_xpath('//*[@id="login-box"]/div/div/div/form/div[2]/div[3]/button').click()  # 登录
+time.sleep(3)
+driver.find_element_by_xpath('//*[@id="menu-item-117007"]/a').click()
+time.sleep(3)
+driver.find_element_by_xpath('//*[@id="main"]/div[2]/div[1]/div[2]/button').click()
 
 driver.quit()

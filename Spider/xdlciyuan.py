fa23c58c989e433e55d8f1b55e5f0f69728aeff5
csong03 <72874056+csong03@users.noreply.github.com>
2021-02-19 00:17:57 +0800

    Update xdlciyuan.py

--- Spider/xdlciyuan.py
+++ Spider/xdlciyuan.py
@@ -18,13 +18,13 @@ login_url = 'https://xdlciyuan.com/'  # 登录URL
 checkin_url = 'https://xdlciyuan.com/mission/today'  # 签到URL
 
 driver.get(login_url)  # 进入登录页面
-driver.find_element_by_xpath('//*[@id="page"]/div[1]/div/div[2]/div/div[2]/div[3]/div/div/div[1]/button[1]').click()  # 使用社区账号登录
+driver.find_element_by_xpath('/html/body/div/div[1]/div/div[2]/div/div[2]/div[3]/div/div/div[1]/button[1]').click()  # 使用社区账号登录
 time.sleep(2)  # 延时加载
 driver.find_element_by_xpath('//*[@id="login-box"]/div/div/div/form/div[2]/label[2]/input').send_keys(NAME)# 填充用户名和密码
 time.sleep(1) 
 driver.find_element_by_xpath('//*[@id="login-box"]/div/div/div/form/div[2]/label[5]/input').send_keys(PASSWORD)
 time.sleep(1) 
-driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/div/div/div/form/div[2]/div[3]/button').click()  # 登录
+driver.find_element_by_xpath('//*[@id="login-box"]/div/div/div/form/div[2]/div[3]/button').click()  # 登录
 time.sleep(3)
 driver.get(checkin_url)
 time.sleep(1)
